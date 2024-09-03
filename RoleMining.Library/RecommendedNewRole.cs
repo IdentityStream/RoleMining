ffe49cdbddf84e135f244f73800186c7b350479a
Dany Gonzalez-Teigland <dany@identitystream.com>
2024-09-03 13:25:07 +0200

    Commit without significance

---
+++ RoleMining.Library/RecommendedNewRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoleMining.Library
+{
+    public class RecommendedNewAccessInRole
+    {
+        public string Role { get; set; }
+        public string Access { get; set; }
+        public int TotalUsers { get; set; }
+        public double ratio { get; set; }
+    }
+}
