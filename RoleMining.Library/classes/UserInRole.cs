cca2b08069da556c4720eba6b3ea896a0de3a242
Dany Gonzalez-Teigland <dany@identitystream.com>
2024-09-04 08:48:38 +0200

    Moved all classes into a folder

---
+++ RoleMining.Library/classes/UserInRole.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoleMining.Library
+{
+    /// <summary>
+    /// Combination of a user and a role
+    /// </summary>
+    public class UserInRole
+    {
+        /// <summary>
+        /// The role ID, unique identifier for each role.
+        /// </summary>
+        public string RoleID { get; set; }
+        /// <summary>
+        /// Unique identifier for each user.
+        /// </summary>
+        public string UserID { get; set; }
+        /// <summary>
+        /// Function to check if this UserInRole object is equal to another object.
+        /// </summary>
+        /// <param name="obj">Object to be compared, usually another <seealso cref="UserInRole">UserInRole</seealso> object.</param>
+        /// <returns>A bool representing if they are equal or not.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is UserInRole other)
+            {
+                return RoleID == other.RoleID && UserID == other.UserID;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Function to return the hash code for the UserInRole object, combination of UserID and RoleID.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return $"{UserID} - {RoleID}".GetHashCode();
+        }
+    }
+}
---
+++ RoleMining.Library/classes/UserRoleSummary.cs
@@ -0,0 +1,17 @@
+﻿namespace RoleMining.Library
+{
+    /// <summary>
+    /// Object used to store how many users have a specific role
+    /// </summary>
+    internal class UserRoleSummary
+    {
+        /// <summary>
+        /// The role ID, unique identifier for each role
+        /// </summary>
+        public string RoleID { get; set; }
+        /// <summary>
+        /// How many users have the specific role
+        /// </summary>
+        public int TotalUsers { get; set; }
+    }
+}
