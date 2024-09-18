de71f3d163030876b5cb058a3012350acd16a6b0
DanyGoT <danygonzteig@gmail.com>
2024-09-18 15:11:01 +0200

    First iteration of a clustering algorithm working

---
+++ RoleMining.Library/Clustering/Kcluster.cs
@@ -0,0 +1,123 @@
+﻿using RoleMining.Library.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace RoleMining.Library.Clustering
+{
+    public class Kcluster
+    {
+        public static List<UserCluster> Cluster(IEnumerable<UserAccess> userAccesses, int similarityThreshold = 1)
+        {
+            InputValidator.CheckIfEmpty(userAccesses, nameof(userAccesses));
+
+            var userAccessVectors = new List<UserAccessVector>();
+
+            var uniqueAccesses = userAccesses.Select(uA => uA.AccessID).Distinct().ToList();
+            var uniqueUsers = userAccesses.Select(uA => uA.UserID).Distinct().ToList();
+
+
+            foreach (var user in uniqueUsers)
+            {
+                var userVector = new UserAccessVector{UserID = user, AccessVector = new BitArray(uniqueAccesses.Count())};
+
+                foreach (var access in uniqueAccesses)
+                {
+                    if (userAccesses.Any(uA => uA.UserID == user && uA.AccessID == access))
+                    {
+                        userVector.AccessVector[uniqueAccesses.IndexOf(access)] = true;
+                    }
+                    else
+                    {
+                        userVector.AccessVector[uniqueAccesses.IndexOf(access)] = false;
+                    }
+                }
+                userAccessVectors.Add(userVector);
+            }
+
+            // Manually cluster
+            // As of now, first creates a cluster around the first user. If another user is outside the
+            // similarity threshold, a new cluster is created. We check the user at each cluster
+            var clusters = new List<UserCluster>();
+
+            foreach (var user in userAccessVectors)
+            {
+                bool addedToCluster = false;
+
+                foreach (var cluster in clusters)
+                {
+                    // Compare to the first user in the cluster
+                    if  (HammingDistance(user.AccessVector, cluster.AccessVector) < similarityThreshold)
+                    {
+
+                        cluster.Users.Add(user);
+                        cluster.UsersInCluster.Add(user.UserID);
+
+                        addedToCluster = true;
+                        break;
+                    }
+                }
+
+                if (!addedToCluster)
+                {
+                        var accessesInCluster = new List<string>();
+                        var amountOfAccessesInCluster = 0;
+                        foreach (var access in uniqueAccesses)
+                        {
+                            if (user.AccessVector[uniqueAccesses.IndexOf(access)] == true)
+                            {
+                                accessesInCluster.Add(access);
+                                amountOfAccessesInCluster++;
+                            }
+                        }
+                    // Create a new cluster if no similar cluster is found
+                    var userList = new List<UserAccessVector> { user };
+                    clusters.Add(new UserCluster
+                    {
+                        Accesses = accessesInCluster,
+                        AmountOfAccesses = amountOfAccessesInCluster,
+                        UsersInCluster = userList.Select(uL => uL.UserID).ToList(),
+                        Users = userList,
+                        AccessVector = user.AccessVector
+                    });
+                }
+            }
+
+            return clusters;
+        }
+
+        public static int HammingDistance(BitArray a, BitArray b)
+    {
+        if (a.Length != b.Length)
+            {
+                throw new ArgumentException("The two BitArrays must be of the same length.");
+            }
+
+        var distance = 0;
+        for (var i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+            {
+                distance++;
+            }
+        }
+        return distance;
+    }
+    public class UserAccessVector
+    {
+        public string UserID { get; set; }
+        public BitArray AccessVector { get; set; } // BitArray for binary role presence
+    }
+    public class UserCluster
+    {
+        public List<string> Accesses { get; set; }
+        public int AmountOfAccesses  { get; set; }
+        public List<string> UsersInCluster { get; set; }
+
+        public List<UserAccessVector> Users { get; set; }
+        public BitArray AccessVector { get; set; }
+
+    }
+    }
+}
---
+++ RoleMining.Tests/KclusterTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using RoleMining.Library;
+using RoleMining.Library.Classes;
+using RoleMining.Library.Algorithms;
+using RoleMining.Library.Clustering;
+
+namespace RoleMining.Tests;
+
+public class KclusterTest : IAlgorithmTest
+{
+    [Fact]
+    public void RunCode()
+    {
+        var userAccess = new List<UserAccess>
+        {
+            new UserAccess { UserID = "2", AccessID = "Write" },
+            new UserAccess { UserID = "1", AccessID = "Read"  },
+            new UserAccess { UserID = "1", AccessID = "Write" },
+            new UserAccess { UserID = "3", AccessID = "Read"  },
+            new UserAccess { UserID = "4", AccessID = "Write" },
+            new UserAccess { UserID = "5", AccessID = "Read"  },
+            new UserAccess { UserID = "6", AccessID = "Write" },
+            new UserAccess { UserID = "7", AccessID = "Read"  },
+            new UserAccess { UserID = "8", AccessID = "Write" }
+        };
+
+        var result = Kcluster.Cluster(userAccess);
+
+        Assert.Equal(1, 1);
+    }
+
+    [Fact]
+    public void TestNullOrEmptyValues()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void TestBasicFunctionality()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void TestLargeData()
+    {
+        var userInRoles = new List<UserInRole>();
+        var userAccesses = new List<UserAccess>();
+
+        var iterations = 10000;
+
+        // Assign 10,000 users access to "read"
+        for (int i = 1; i <= iterations; i++)
+        {
+            userAccesses.Add(new UserAccess { UserID = i.ToString(), AccessID = $"{i}" });
+        }
+
+        var result = Kcluster.Cluster(userAccesses);
+        Assert.Equal(1, 1);  // Expecting 1 role with 10,000 users
+    }
+}
