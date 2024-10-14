0849fd0ef1fb18552cc98067cfe3e6ce9e12e17c
DanyGoT <danygonzteig@gmail.com>
2024-10-14 12:55:55 +0200

    added documentation for servicecCollectionExtensions

--- RoleMining.Library/ServiceCollectionExtensions.cs
+++ RoleMining.Library/ServiceCollectionExtensions.cs
@@ -3,12 +3,21 @@ using RoleMining.Library.Algorithms;
 
 namespace RoleMining.Library
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="IServiceCollection"/> to add RoleMining services.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Method to add RoleMining algorithm services to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="algorithm"></param>
+        /// <param name=""></param>
         public static void AddRoleMining(
             this IServiceCollection services,
-            RecommenderAlgorithm algorithm = RecommenderAlgorithm.JaccardIndex,
-            RoleBuilderAlgorithm roleBuilderAlgorithm = RoleBuilderAlgorithm.Kcluster)
+            RecommenderAlgorithm algorithm = RecommenderAlgorithm.JaccardIndex
+            )
         {
             switch (algorithm)
             {
